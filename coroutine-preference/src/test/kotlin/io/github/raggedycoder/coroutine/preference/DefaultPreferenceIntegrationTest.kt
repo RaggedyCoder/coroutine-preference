87d9ac1ab4afce526d202be75c93d1d0c3380754
raggedycoder <sajid.sust.cse@gmail.com>
2020-09-11 15:24:01 +0700

    Prepare for release 0.1.0

--- coroutine-preference/src/test/kotlin/io/github/raggedycoder/coroutine/preference/DefaultPreferenceIntegrationTest.kt
+++ coroutine-preference/src/test/kotlin/io/github/raggedycoder/coroutine/preference/DefaultPreferenceIntegrationTest.kt
@@ -1,13 +1,25 @@
+/*
+ * Copyright 2020 RaggedyCoder.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.raggedycoder.coroutine.preference
 
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.test.core.app.ApplicationProvider
 import com.google.gson.Gson
-import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
