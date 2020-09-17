de255b59f33978720d89ee40c78c236b93eb85cb
raggedycoder <sajid.sust.cse@gmail.com>
2020-09-17 13:57:39 +0700

    Added documentation to the interfaces.

--- coroutine-preference/src/main/kotlin/io/github/raggedycoder/coroutine/preference/CoroutineSharedPreferences.kt
+++ coroutine-preference/src/main/kotlin/io/github/raggedycoder/coroutine/preference/CoroutineSharedPreferences.kt
@@ -27,45 +27,107 @@ import io.github.raggedycoder.coroutine.preference.adapter.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import java.lang.reflect.Type
 
+/**
+ * Interface for accessing and modifying preference data returned from SharedPreferences. It's a
+ * wrapper of default android SharedPreferences which will wrap a preference it an interface that
+ * can provide the data as flowable.
+ */
 @ExperimentalCoroutinesApi
 interface CoroutineSharedPreferences {
 
+    /**
+     * Provides a boolean preference which is mapped with a key in SharedPreferences.
+     * @param key The name of the preference to retrieve.
+     * @param defaultValue Value to return if this preference does not exist.
+     * @param adapter The adapter which is used to retrieve the data from the SharedPreference and
+     * mapped to their original domain model.
+     * @return The instance of the Preference
+     */
     fun getBoolean(
         key: String,
         defaultValue: Boolean = DEFAULT_BOOLEAN,
         adapter: Adapter<Boolean> = BooleanAdapter
     ): Preference<Boolean>
 
+    /**
+     * Provides an int preference which is mapped with a key in SharedPreferences.
+     * @param key The name of the preference to retrieve.
+     * @param defaultValue Value to return if this preference does not exist.
+     * @param adapter The adapter which is used to retrieve the data from the SharedPreference and
+     * mapped to their original domain model.
+     * @return The instance of the Preference
+     */
     fun getInt(
         key: String,
         defaultValue: Int = DEFAULT_INT,
         adapter: Adapter<Int> = IntAdapter
     ): Preference<Int>
 
+    /**
+     * Provides a long preference which is mapped with a key in SharedPreferences.
+     * @param key The name of the preference to retrieve.
+     * @param defaultValue Value to return if this preference does not exist.
+     * @param adapter The adapter which is used to retrieve the data from the SharedPreference and
+     * mapped to their original domain model.
+     * @return The instance of the Preference
+     */
     fun getLong(
         key: String,
         defaultValue: Long = DEFAULT_LONG,
         adapter: Adapter<Long> = LongAdapter
     ): Preference<Long>
 
+    /**
+     * Provides a float preference which is mapped with a key in SharedPreferences.
+     * @param key The name of the preference to retrieve.
+     * @param defaultValue Value to return if this preference does not exist.
+     * @param adapter The adapter which is used to retrieve the data from the SharedPreference and
+     * mapped to their original domain model.
+     * @return The instance of the Preference
+     */
     fun getFloat(
         key: String,
         defaultValue: Float = DEFAULT_FLOAT,
         adapter: Adapter<Float> = FloatAdapter
     ): Preference<Float>
 
+    /**
+     * Provides a string preference which is mapped with a key in SharedPreferences.
+     * @param key The name of the preference to retrieve.
+     * @param defaultValue Value to return if this preference does not exist.
+     * @param adapter The adapter which is used to retrieve the data from the SharedPreference and
+     * mapped to their original domain model.
+     * @return The instance of the Preference
+     */
     fun getString(
         key: String,
         defaultValue: String = DEFAULT_STRING,
         adapter: Adapter<String> = StringAdapter
     ): Preference<String>
 
+    /**
+     * Provides a string set preference which is mapped with a key in SharedPreferences.
+     * @param key The name of the preference to retrieve.
+     * @param defaultValue Value to return if this preference does not exist.
+     * @param adapter The adapter which is used to retrieve the data from the SharedPreference and
+     * mapped to their original domain model.
+     * @return The instance of the Preference
+     */
     fun getStringSet(
         key: String,
         defaultValue: Set<String> = DEFAULT_STRING_SET,
         adapter: Adapter<Set<String>> = StringSetAdapter
     ): Preference<Set<String>>
 
+    /**
+     * Provides an enum preference which is mapped with a key in SharedPreferences.
+     * @param key The name of the preference to retrieve.
+     * @param defaultValue Value to return if this preference does not exist.
+     * @param enumClass Instances of the class that represent the enum class
+     * @param adapter The adapter which is used to retrieve the data from the SharedPreference and
+     * mapped to their original domain model.
+     * @return The instance of the Preference
+     */
     fun <T : Enum<T>> getEnum(
         key: String,
         defaultValue: T,
@@ -73,6 +135,15 @@ interface CoroutineSharedPreferences {
         adapter: Adapter<T> = DefaultEnumAdapterCache.getDefault(defaultValue, enumClass)
     ): Preference<T>
 
+    /**
+     * Provides an object preference which is mapped with a key in SharedPreferences.
+     * @param key The name of the preference to retrieve.
+     * @param defaultValue Value to return if this preference does not exist.
+     * @param objectClass Instances of the class that represent the object class
+     * @param adapter The adapter which is used to retrieve the data from the SharedPreference and
+     * mapped to their original domain model.
+     * @return The instance of the Preference
+     */
     fun <T> getObject(
         key: String,
         defaultValue: T,
@@ -80,6 +151,15 @@ interface CoroutineSharedPreferences {
         adapter: Adapter<T> = DefaultObjectAdapterCache.getDefault(defaultValue, objectClass)
     ): Preference<T>
 
+    /**
+     * Provides an object preference which is mapped with a key in SharedPreferences.
+     * @param key The name of the preference to retrieve.
+     * @param defaultValue Value to return if this preference does not exist.
+     * @param objectType Type of the common superinterface of the object
+     * @param adapter The adapter which is used to retrieve the data from the SharedPreference and
+     * mapped to their original domain model.
+     * @return The instance of the Preference
+     */
     fun <T> getObject(
         key: String,
         defaultValue: T,
@@ -87,20 +167,36 @@ interface CoroutineSharedPreferences {
         adapter: Adapter<T> = DefaultObjectAdapterCache.getDefault(defaultValue, objectType)
     ): Preference<T>
 
+    /**
+     * Removes all the data stored in the SharedPreferences.
+     */
     fun clear()
 
+    /**
+     * The builder class to build a default implementation of the CoroutineSharedPreferences.
+     */
     class Builder {
         private var sharedPreferences: SharedPreferences? = null
 
+        /**
+         * Saves the instance of SharedPreferences in which the preference will be saved or
+         * retrieved.
+         * @param sharedPreferences The SharedPreferences.
+         */
         fun preferences(sharedPreferences: SharedPreferences): Builder {
             this.sharedPreferences = sharedPreferences
             return this
         }
 
+        /**
+         * Builds and returns the default implementation of the CoroutineSharedPreferences.
+         * @return implementation of CoroutineSharedPreferences
+         * @throws NullPointerException If no implementation of SharedPreferences isn't provided
+         * via preferences(#SharedPreferences) it will throw a null pointer exception
+         */
         fun build(): CoroutineSharedPreferences {
-            val sharedPreferences =
-                this.sharedPreferences ?: throw NullPointerException("sharedPreferences == null")
-            return DefaultCoroutineSharedPreferences(sharedPreferences)
+            return this.sharedPreferences?.let { DefaultCoroutineSharedPreferences(it) }
+                ?: throw NullPointerException("sharedPreferences == null")
         }
     }
 }
--- coroutine-preference/src/main/kotlin/io/github/raggedycoder/coroutine/preference/Preference.kt
+++ coroutine-preference/src/main/kotlin/io/github/raggedycoder/coroutine/preference/Preference.kt
@@ -18,23 +18,75 @@ package io.github.raggedycoder.coroutine.preference
 import android.content.SharedPreferences
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Interface for accessing and modifying preference data stored in Sharedpreferences.
+ * It can supply the data directly also as a flowable implementation which will always provide new
+ * data whenever the stored data is modified inside the SharedPreferences.
+ * @see SharedPreferences
+ * @see SharedPreferences.Editor
+ */
 interface Preference<T> {
+
+    /**
+     * Adapters are the gateway to get and set a value to a preference. The purpose of the adapter
+     * is values can be stored in SharedPreferences in a different domain object. It will do those
+     * mapping to get & set the data.
+     */
     interface Adapter<T> {
+        /**
+         * Retrieves the value from the preferences.
+         * @param key The name of the preference to retrieve.
+         * @param preferences The preference from where the data will be retrieved.
+         * @return The retrieved value in the preference
+         */
         fun get(key: String, preferences: SharedPreferences): T
+
+        /**
+         * Sets the value in the preferences editor, to be written back once.
+         * {@link #commit} or {@link #apply} are called.
+         *  @param key The name of the preference to modify.
+         *  @param value The new value for the preference.
+         *  @param editor The Interface that will be used for modifying values in a
+         *  SharedPreferences object.
+         */
         fun set(key: String, value: T, editor: SharedPreferences.Editor)
     }
 
+    /**
+     * The name of the preference to retrieve or modify in SharedPreferences.
+     */
     val key: String
 
+    /**
+     * The value that will be provided if the value is not available.
+     */
     val defaultValue: T
 
+    /**
+     * The stored value that was retrieved from the SharedPreferences.
+     */
     val value: T
 
+    /**
+     * The flowable instance which will provide a new value whenever the data is modified in
+     * SharedPreference
+     */
     val valueAsFlow: Flow<T>
 
+    /**
+     * Checks whether the SharedPreferences contains a preference.
+     */
     val available: Boolean
 
+    /**
+     * Sets a new value in the preferences editor,
+     * @param value The new value for the preference. Passing null for this argument is
+     * equivalent to calling delete().
+     */
     fun set(value: T?)
 
+    /**
+     * Removes the stored value of the preference from the SharedPreferences
+     */
     fun delete()
 }
